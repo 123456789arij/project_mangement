9fd0e0dc3428ddad5dd32d9d41c32980eee39e31
123456789arij <abidarij1@gmail.com>
2020-04-13 15:17:51 +0100

    integration_template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,9 +61,26 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            body, html {
+                height: 100%;
+            }
+
+            .bg {
+                /* The image used */
+                background-image: url("photo.jpg");
+
+                /* Full height */
+                height: 100%;
+
+                /* Center and scale the image nicely */
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+
         </style>
     </head>
-    <body>
+    <body class="bg">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -80,20 +97,9 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+
+
             </div>
         </div>
     </body>
