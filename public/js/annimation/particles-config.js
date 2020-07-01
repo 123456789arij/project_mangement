992e4497c3abb54850fcf0344dad6c92a8a0dd10
123456789arij <abidarij1@gmail.com>
2020-07-01 01:26:30 +0100

    update

---
+++ public/js/annimation/particles-config.js
@@ -0,0 +1,110 @@
+particlesJS('particles-js',{
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 900
+      }
+    },
+    "color": {
+      "value": "#185D8A"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#C2D8E6"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.8403025089032057,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 0.16210739614994935,
+        "opacity_min": 0.24316109422492402,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 4,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 10,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 140,
+      "color": "#E3ECF1",
+      "opacity": 0.9,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 600,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 800,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
---
+++ resources/views/project/category/membre.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts.base')
+@section('cssBlock')
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <style>
+
+    </style>
+@endsection
+@section('content')
+    {{-- app-page-title--}}
+    <div class="app-page-title">
+        <div class="page-title-wrapper">
+            {{-- page-title-wrapper--}}
+            <div class="page-title-heading">
+                <div class="page-title-icon">
+                    <i class='metismenu-icon fas fa-layer-group'></i>
+                </div>
+                <h5> {{ __('messages.project') }} {{$project->id}} #-<strong>{{$project->name}}</strong></h5>
+            </div>
+            {{--   /page-title-wrapper--}}
+
+            <div class="page-title-actions">
+                <div class="d-inline-block dropdown text-center">
+                    <button class="btn-shadow mb-2 mr-2 btn btn-info btn-lg">
+                         <span class="btn-icon-wrapper pr-2 opacity-7">
+                              <i class="pe-7s-note  btn-icon-wrapper" style="font-size: 20px;"></i>
+                          </span>
+                        <a href="{{route('project.edit',$project->id)}}"
+                           style="color: white;font-size: 15px;">   {{__('messages.edit') }}</a>&nbsp;&nbsp;
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--                /app-page-title--}}
+
+    <div class="row">
+
+
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <!------ carde tab ---------->
+            <div class="mb-3 card">
+                <div class="card-header">{{ __('messages.Project_Membres') }}</div>
+                <div class="card-body">
+                    <div class="tab-content">
+                        <div class="row">
+                            {{--                   Project_Membres             --}}
+                            <div class="col-6">
+                                <h4 style="text-align: center">{{ __('messages.Project_Membres') }}</h4>
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">{{ __('messages.name') }}</th>
+                                        <th scope="col">{{ __('messages.role') }}</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($membres as $employee)
+                                        <tr>
+                                            <th scope="row">
+                                                <div class="widget-content p-0">
+                                                    <div class="widget-content-wrapper">
+                                                        <div class="widget-content-left mr-3">
+                                                            <div class="widget-content-left">
+                                                                <img src="{{asset($employee->image)}}"
+                                                                     class="rounded-circle"
+                                                                     height="40px" width="40px" alt="im"/>
+                                                            </div>
+                                                        </div>
+                                                        <div class="widget-content-left flex2">
+                                                            <div class="widget-heading">
+                                                                {{$employee->name }}   </div>
+                                                            {{--                                                                <div class="widget-subheading opacity-7">Web Developer</div>--}}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </th>
+                                            <td style="text-align: justify;">
+                                                @if($employee->role  == 1)
+                                                    <span> Employée</span>
+                                                @else
+                                                    <span>Chef De Projet</span>
+                                                @endif
+
+                                                {{--       <form action="{{ route('destroy_membre',$employee->id) }}"
+                                                                          method="post">.
+                                                                        @csrf
+                                                                        @method('DELETE')
+                                                                        <button
+                                                                            class="mr-2 btn-icon btn-icon-only btn btn-outline-danger">
+                                                                            <i class="pe-7s-trash btn-icon-wrapper"
+                                                                               style="font-size: 20px;"> </i></button>
+
+                                                                    </form>--}}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            {{--                   /Project_Membres             --}}
+                            {{--                                add_Project_Membres     --}}
+                            <div class="col-6">
+                                <h4 style="text-align: center">{{ __('messages.add_Project_Membres') }} </h4>
+                                <form method="POST" action="{{route('membre')}}">
+                                    @csrf
+                                    <input type="hidden" name="project_id" value="{{$project->id}}">
+                                    <select class="mb-2 form-control-lg form-control" name="employee_id[]" multiple>
+                                        @foreach($employees as $employee)
+                                            <option
+                                                value="{{$employee->id}}"{{ in_array($employee->id,$membres->pluck('id')->toArray()) ? 'selected' : '' }}>
+                                                {{$employee->name}} </option>
+                                        @endforeach
+                                    </select>
+                                    <br>
+                                    <button class="btn-wide btn btn-success" type="submit">
+                                        <i class="fas fa-check"></i>
+                                        {{ __('messages.Save') }}
+                                    </button>
+                                </form>
+                            </div>
+                            {{--                               / add_Project_Membres     --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{--                /carde tab--}}
+
+        </div>
+    </div>
+@endsection
+@section('jsBlock')
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+@endsection
